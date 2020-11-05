59f73b85981a7cae16586ccf53d3e2aff11e2275
mossa <mossann0510@gmail.com>
2020-11-06 00:39:19 +0900

    demo-test

---
+++ key.js
@@ -0,0 +1,1 @@
+window.__SKYWAY_KEY__ = '9105f624-cf34-4b23-96e0-21f697b6fb92';
--- main.js
+++ main.js
@@ -1,49 +1,123 @@
+const Peer = window.Peer;
 
-const peer = new Peer({
-    key: 'a1c5acb4-37fc-46f6-b239-0cd0017e60e8',
-    debug: 3
-});
+(async function main() {
+  const localVideo = document.getElementById('js-local-stream');
+  const localId = document.getElementById('js-local-id');
+  const callTrigger = document.getElementById('js-call-trigger');
+  const closeTrigger = document.getElementById('js-close-trigger');
+  const remoteVideo = document.getElementById('js-remote-stream');
+  const remoteId = document.getElementById('js-remote-id');
+  const meta = document.getElementById('js-meta');
+  const sdkSrc = document.querySelector('script[src*=skyway]');
 
-peer.on('open', () => {
-    document.getElementById('my-id').textContent = peer.id;
-});
+  meta.innerText = `
+    UA: ${navigator.userAgent}
+    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+  `.trim();
+  let localStream;
 
-//着信処理
-peer.on('call', mediaConnection => {
+  const micAudio = new Tone.UserMedia();
+  // マイクがオープンしたときのコールバック関数にgetUserMediaを格納
+  micAudio.open().then( () => {
+    const Numpitch = Math.floor(Math.random()*(10+10)-10);
+    const shifter = new Tone.PitchShift(Numpitch);
+    const reverb = new Tone.Freeverb();
+    // 加工済みの音声を受け取る空のノードを用意
+    const effectedDest = Tone.context.createMediaStreamDestination();
+    micAudio.connect(shifter);
+    shifter.connect(reverb);
+    // リバーブを空のノードに接続
+    reverb.connect(effectedDest);
+
+    // カメラ映像取得
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+      .then( stream => {
+
+      // ストリームから音声トラックを削除
+      const oldTrack = stream.getAudioTracks()[0];
+      stream.removeTrack(oldTrack);
+
+      // ストリームにエフェクトがかかった音声トラックを追加
+      const effectedTrack = effectedDest.stream.getAudioTracks()[0];
+      stream.addTrack(effectedTrack); 
+
+      // 成功時にvideo要素にカメラ映像をセットし、再生
+      // const videoElm = document.getElementById('my-video')
+      localVideo.muted = true;
+      localVideo.srcObject = stream;
+      localVideo.play();
+      // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+      localStream = stream;
+    }).catch( error => {
+      // 失敗時にはエラーログを出力
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
+  });
+
+  // const localStream = await navigator.mediaDevices
+  //   .getUserMedia({
+  //     audio: true,
+  //     video: true,
+  //   })
+  //   .catch(console.error);
+
+  // Render local stream
+  // localVideo.muted = true;
+  // localVideo.srcObject = localStream;
+  // localVideo.playsInline = true;
+  // await localVideo.play().catch(console.error);
+
+  const peer = (window.peer = new Peer({
+    key: window.__SKYWAY_KEY__,
+    debug: 3,
+  }));
+
+  // Register caller handler
+  callTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const mediaConnection = peer.call(remoteId.value, localStream);
+
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for caller
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
+    });
+
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+  });
+
+  peer.once('open', id => (localId.textContent = id));
+
+  // Register callee handler
+  peer.on('call', mediaConnection => {
     mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
-});
 
-// 発信処理
-document.getElementById('make-call').onclick = () => {
-    const theirID = document.getElementById('their-id').value;
-    const mediaConnection = peer.call(theirID, localStream);
-    setEventListener(mediaConnection);
-};
-
-// イベントリスナを設置する関数
-const setEventListener = mediaConnection => {
-    mediaConnection.on('stream', stream => {
-        // video要素にカメラ映像をセットして再生
-        const videoElm = document.getElementById('their-video')
-        videoElm.srcObject = stream;
-        videoElm.play();
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for callee
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
     });
-}
 
-let localStream;
-
-// カメラ映像取得
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then(stream => {
-        // 成功時にvideo要素にカメラ映像をセットし、再生
-        const videoElm = document.getElementById('my-video');
-        videoElm.srcObject = stream;
-        videoElm.play();
-        // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-        localStream = stream;
-    }).catch(error => {
-        // 失敗時にはエラーログを出力
-        console.error('mediaDevice.getUserMedia() error:', error);
-        return;
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
     });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+  });
+
+  peer.on('error', console.error);
+})();
